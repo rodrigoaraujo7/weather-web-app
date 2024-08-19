dd61f64591d6bd77a2d7d78293669e7e1efde832
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2024-08-18 23:22:32 -0300

    feat: configuring dotenv variable and fetching weather api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+  const search = async (city: string) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`; // Atualize aqui
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    search("London");
+  }, []);
+
   return (
-    <h1>Hello World!</h1>
+    <h1>{process.env.REACT_APP_WEATHER_API_KEY}</h1>
   );
 }
 
